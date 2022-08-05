0775c7d6e2c58da838e1c2731f4363f5e970c96a
병진 <zzand777@naver.com>
2022-08-05 20:49:31 +0900

    상태 추가

---
+++ src/main/java/com/example/graphql/api/dao/TbmRmEptEquipStateRepository.java
@@ -0,0 +1,14 @@
+package com.example.graphql.api.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.graphql.api.entity.TbmRmEptEquipState;
+import com.example.graphql.api.entity.TbmRmEptEquipStatePK;
+
+@Repository
+public interface TbmRmEptEquipStateRepository extends JpaRepository<TbmRmEptEquipState, TbmRmEptEquipStatePK> {
+    public List<TbmRmEptEquipState> findByFctCode(String fctCode);
+}
