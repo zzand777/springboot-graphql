e7693a97872d48c7dad315227c9e427fe3abf999
병진 <zzand777@naver.com>
2022-08-09 21:11:55 +0900

    설비 상태 서비스 추가

--- src/main/java/com/example/graphql/api/service/EquipStateService.java
+++ src/main/java/com/example/graphql/api/service/EquipStateService.java
@@ -1,5 +1,11 @@
 package com.example.graphql.api.service;
 
+import java.util.List;
+
+import com.example.graphql.api.dto.TbmRmEptEquipStateDTO;
+
 public interface EquipStateService {
-    
+    TbmRmEptEquipStateDTO findById(TbmRmEptEquipStateDTO equipState);
+    List<TbmRmEptEquipStateDTO> findByFctCode(TbmRmEptEquipStateDTO equipState);
+    List<TbmRmEptEquipStateDTO> saveAll(List<TbmRmEptEquipStateDTO> equipState);
 }
--- src/main/java/com/example/graphql/api/service/impl/EquipStateServiceImpl.java
+++ src/main/java/com/example/graphql/api/service/impl/EquipStateServiceImpl.java
@@ -1,5 +1,46 @@
 package com.example.graphql.api.service.impl;
 
-public class EquipStateServiceImpl {
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.graphql.api.dto.TbmRmEptEquipStateDTO;
+import com.example.graphql.api.entity.TbmRmEptEquipState;
+import com.example.graphql.api.entity.TbmRmEptEquipStatePK;
+import com.example.graphql.api.repository.TbmRmEptEquipStateRepository;
+import com.example.graphql.api.service.EquipStateService;
+
+@Service
+@Transactional
+public class EquipStateServiceImpl implements EquipStateService {
+
+    @Autowired
+    private TbmRmEptEquipStateRepository tbmRmEptEquipStateRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Override
+    public TbmRmEptEquipStateDTO findById(TbmRmEptEquipStateDTO equipState) {
+        return tbmRmEptEquipStateRepository.findById(modelMapper.map(equipState, TbmRmEptEquipStatePK.class))
+        .map(v -> modelMapper.map(v, TbmRmEptEquipStateDTO.class)).orElse(null);
+    }
+
+    @Override
+    public List<TbmRmEptEquipStateDTO> findByFctCode(TbmRmEptEquipStateDTO equipState) {
+        return tbmRmEptEquipStateRepository.findByFctCode(equipState.getFctCode())
+        .stream().map(v -> modelMapper.map(v, TbmRmEptEquipStateDTO.class)).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<TbmRmEptEquipStateDTO> saveAll(List<TbmRmEptEquipStateDTO> equipState) {
+        return tbmRmEptEquipStateRepository.saveAll(equipState.stream().map(v -> modelMapper.map(v, TbmRmEptEquipState.class)).collect(Collectors.toList()))
+        .stream().map(v -> modelMapper.map(v, TbmRmEptEquipStateDTO.class)).collect(Collectors.toList());
+
+    }
 
 }
